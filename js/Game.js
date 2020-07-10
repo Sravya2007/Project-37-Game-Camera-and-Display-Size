835fc0ce2567d0f856eba03acfb944674729cf36
Sravya2007 <sravyamarepally@outlook.com>
2020-07-10 18:04:06 +0530

    New Game

--- js/Game.js
+++ js/Game.js
@@ -26,30 +26,43 @@ class Game {
       form = new Form()
       form.display();
     }
+    car1 = createSprite(100, 200, 25, 25);
+    car1.shapeColor = "blue";
+    car2 = createSprite(300, 200, 50, 50);
+    car1.shapeColor = "green";
+    car3 = createSprite(500, 200, 75, 75);
+    car1.shapeColor = "yellow";
+    car4 = createSprite(700, 200, 100, 100);
+    car1.shapeColor = "pink";
+    cars = [car1, car2, car3, car4];
   }
 
   play(){
     form.hide();
-    textSize(30);
-    text("Game Start", 120, 100)
     Player.getPlayerInfo();
 
     if(allPlayers !== undefined){
       var display_position = 130;
+      //index of the array
+      var index = 0;
+      var x = 0, y = 0;
       for(var plr in allPlayers){
-        if (plr === "player" + player.index)
-          fill("red")
-        else
-          fill("black");
+        index = index + 1;
+        x = x + 200;
+        y = displayHeight - allPlayers[plr].distance
+        cars[index - 1].x = x
+        cars[index - 1].y = y
 
-        display_position+=20;
-        textSize(15);
-        text(allPlayers[plr].name + ": " + allPlayers[plr].distance, 120,display_position)
+        if (index === player.index) {
+          cars[index - 1].shapeColor = "red";
+          camera.position.x = displayWidth/2;
+          camera.position.y = cars[index - 1].y;
+        }         
       }
     }
 
     if(keyIsDown(UP_ARROW) && player.index !== null){
-      player.distance +=50
+      player.distance += 50
       player.update();
     }
   }
